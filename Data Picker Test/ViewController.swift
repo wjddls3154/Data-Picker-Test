ea571cd25495d1e32bf73f49dc345c89a81914b8
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-11 15:47:06 +0900

    Date Picker

--- Data Picker Test/ViewController.swift
+++ Data Picker Test/ViewController.swift
@@ -9,25 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var myDatePicker: UIDatePicker!
-    @IBOutlet weak var timeLabel: UILabel!
-    
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    @IBOutlet weak var lblPickerTime: UILabel!
+    @IBOutlet weak var lblCurrentTime: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    @IBAction func changeDatePicker(_ sender: Any) {
-        print("change Date Picker")
-        //        print(myDatePicker.date)
         
-        // dateFormat
+         Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)    }
+    
+  
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss a EE"
-        timeLabel.text = formatter.string(from: myDatePicker.date)
-        
-        //        timeLabel.text = String(describing: myDatePicker.date)    }
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+      
     }
-    
+    @objc func updateTime() {
+        //        lblCurrentTime.text = String(count)
+        //        count = count + 1
+        
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+    }
 }
 
